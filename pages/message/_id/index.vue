cf5e70159125ba0d428a36eb3602ea101745652e
konu96 <ukouhei96@gmail.com>
2020-12-06 14:44:48 +0900

    [ADD] API と通信が成功した時に、遷移するページのコンポーネントを追加

---
+++ pages/message/_id/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="section no-top-pad">
+    詳細ページ
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  
+});
+</script>
+
+<style scoped>
+
+</style>
